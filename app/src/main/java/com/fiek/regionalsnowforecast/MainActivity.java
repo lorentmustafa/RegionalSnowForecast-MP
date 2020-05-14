6da134943650ff82ac547328cfa96fc3a4bd2ae8
Leart Mekolli <44322678+LeartMekolli@users.noreply.github.com>
2020-05-14 17:42:31 +0200

    Login UI
    Login UI and login button functionality.
    Adding components from Adobe XD.

--- app/src/main/java/com/fiek/regionalsnowforecast/MainActivity.java
+++ app/src/main/java/com/fiek/regionalsnowforecast/MainActivity.java
@@ -2,13 +2,28 @@ package com.fiek.regionalsnowforecast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btnLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnLogin = findViewById(R.id.btnLogin);
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
